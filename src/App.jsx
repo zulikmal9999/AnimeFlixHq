46e996b1a75116a60cbc7f2059d65c1dfbdde907
Zack <a.zulikmal@gmail.com>
2025-05-05 23:21:54 +0800

    Refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home"; // We'll move your current App logic here
-import AnimeInfo from "./pages/AnimeInfo"; // Assuming AnimeInfo is placed in /pages
+import Home from "./pages/Home"; 
+import AnimeInfo from "./pages/AnimeInfo"; 
 
 const App = () => {
   return (
